b532d7b2e9c83664b1a5518a64dc3bc450e5e93f
Amanda Cole <col22031@byui.edu>
2023-11-17 16:43:05 -0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace BookStoreApi.Models;
+
+public class Book
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+
+    [BsonElement("Name")]
+    [JsonPropertyName("Name")]
+    public string BookName { get; set; } = null!;
+
+    [JsonPropertyName("Price")]
+    public decimal Price { get; set; }
+
+    [JsonPropertyName("Category")]
+    public string Category { get; set; } = null!;
+
+    [JsonPropertyName("Author")]
+    public string Author { get; set; } = null!;
+}
